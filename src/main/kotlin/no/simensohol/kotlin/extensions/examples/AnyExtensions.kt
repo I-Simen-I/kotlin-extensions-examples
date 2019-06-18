e08b526cd78eee328321136ec539486976223ec8
Simen Søhol <simen.so@outlook.com>
2019-06-18 19:54:41 +0200

    La til extensions på any

---
+++ src/main/kotlin/no/simensohol/kotlin/extensions/examples/AnyExtensions.kt
@@ -0,0 +1,5 @@
+package no.simensohol.kotlin.extensions.examples
+
+fun Any?.isNull(): Boolean = this == null
+
+fun Any?.isNotNull() = this.isNull().not()
---
+++ src/test/kotlin/no/simensohol/kotlin/extensions/examples/AnyExtensionsTest.kt
@@ -0,0 +1,44 @@
+package no.simensohol.kotlin.extensions.examples
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class AnyExtensionsTest {
+    @Test
+    fun `Test isNull() is true`() {
+        val nullString: String? = null
+        val nullInt: Int? = null
+        val nullList: List<Any>? = null
+
+        assertThat(null.isNull()).isTrue()
+        assertThat(nullString.isNull()).isTrue()
+        assertThat(nullInt.isNull()).isTrue()
+        assertThat(nullList.isNull()).isTrue()
+    }
+
+    @Test
+    fun `Test isNull() is false`() {
+        assertThat("".isNull()).isFalse()
+        assertThat(1.isNull()).isFalse()
+        assertThat(listOf("", "").isNull()).isFalse()
+    }
+
+    @Test
+    fun `Test isNotNull() is true`() {
+        assertThat("".isNotNull()).isTrue()
+        assertThat(1.isNotNull()).isTrue()
+        assertThat(listOf("", "").isNotNull()).isTrue()
+    }
+
+    @Test
+    fun `Test isNotNull() is false`() {
+        val nullString: String? = null
+        val nullInt: Int? = null
+        val nullList: List<Any>? = null
+
+        assertThat(null.isNotNull()).isFalse()
+        assertThat(nullString.isNotNull()).isFalse()
+        assertThat(nullInt.isNotNull()).isFalse()
+        assertThat(nullList.isNotNull()).isFalse()
+    }
+}
